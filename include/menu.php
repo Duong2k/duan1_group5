e9dcdc64407d13cd788d3792e66978bb561b7573
Văn Dương <boday382033@gmail.com>
2022-11-24 03:15:44 +0700

    update

---
+++ include/menu.php
@@ -0,0 +1,84 @@
+<?php 
+		$sql_category = mysqli_query($con,'SELECT * FROM tbl_category ORDER BY category_id DESC');
+	?>
+<div class="navbar-inner">
+		<div class="container">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<div class="agileits-navi_search">
+					<form action="#" method="post">
+						<select id="agileinfo-nav_search" name="agileinfo_search" class="border" required="">
+							<option value="">Danh mục sản phẩm</option>
+							<?php
+							while($row_category = mysqli_fetch_array($sql_category)) {
+							?>
+							<option value="<?php echo $row_category['category_id'] ?>"><?php echo $row_category['category_name'] ?></option>
+							<?php
+							 }
+							?>
+						</select>
+					</form>
+				</div>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+				    aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto text-center mr-xl-5">
+						<li class="nav-item active mr-lg-2 mb-lg-0 mb-2">
+							<a class="nav-link" href="index.php">Trang chủ
+								<span class="sr-only">(current)</span>
+							</a>
+						</li>
+						<?php 
+							$sql_category_danhmuc = mysqli_query($con,'SELECT * FROM tbl_category ORDER BY category_id DESC');
+							while($row_category_danhmuc = mysqli_fetch_array($sql_category_danhmuc)){
+						?>
+						<li class="nav-item  mr-lg-2 mb-lg-0 mb-2">
+
+							<a class="nav-link " href="?quanly=danhmuc&id=<?php echo $row_category_danhmuc['category_id'] ?>" role="button"  aria-haspopup="true" aria-expanded="false">
+								<?php echo $row_category_danhmuc['category_name'] ?>
+							</a>
+							
+						</li>
+						<?php
+						} 
+						?>
+						<li class="nav-item dropdown mr-lg-2 mb-lg-0 mb-2">
+							<?php
+							$sql_danhmuctin = mysqli_query($con,"SELECT * FROM tbl_danhmuc_tin ORDER BY danhmuctin_id DESC"); 
+
+							?>
+							<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								Tin tức
+							</a>
+							<div class="dropdown-menu">
+								<?php
+								while($row_danhmuctin = mysqli_fetch_array($sql_danhmuctin)){ 
+								?>
+								<a class="dropdown-item" href="?quanly=tintuc&id_tin=<?php echo $row_danhmuctin['danhmuctin_id'] ?>"><?php echo $row_danhmuctin['tendanhmuc'] ?></a>
+								<?php
+								} 
+								?>
+							</div>
+						</li>
+						<li class="nav-item dropdown mr-lg-2 mb-lg-0 mb-2">
+							<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								Trang
+							</a>
+							<div class="dropdown-menu">
+								<a class="dropdown-item" href="product.html">Sản phẩm mới</a>
+
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="checkout.html">Kiểm tra hàng</a>
+								<a class="dropdown-item" href="payment.html">Thanh toán</a>
+							</div>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="contact.html">Liên hệ</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</div>
+	<!-- //navigation -->
---
+++ include/slider.php
@@ -0,0 +1,37 @@
+<!-- banner -->
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		<!-- Indicators-->
+		<ol class="carousel-indicators">
+			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+		</ol>
+		<div class="carousel-inner">
+			<?php
+			$sql_slider = mysqli_query($con,"SELECT * FROM tbl_slider WHERE slider_active='1' ORDER BY slider_id");
+			while($row_slider = mysqli_fetch_array($sql_slider)){ 
+			?>
+			<div class="carousel-item item1 active">
+				<div class="container">
+					<div class="w3l-space-banner">
+						<div class="carousel-caption p-lg-5 p-sm-4 p-3">
+							<p><?php echo $row_slider['slider_caption'] ?></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?php
+			} 
+			?>
+		</div>
+		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+	<!-- //banner -->
